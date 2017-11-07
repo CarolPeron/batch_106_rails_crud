57a05b2da6b909570d0e58e6151429d0f9dc1252
Roberto Barros <roberto.f.barros@gmail.com>
2017-11-07 10:58:06 -0200

    rails CRUD

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+r1 = Restaurant.new(name: 'Paladar da Vila', address: 'Mourato Coelho', stars: 5)
+
+r1.save
+
+r2 = Restaurant.new(name: 'Beijupira', address: 'Recife', stars: 1)
+r2.save
+
+r3 = Restaurant.new(name: 'Bulguer', address: 'Fradique', stars: 4)
+r3.save
